b3748fb7ac378d32c66f33410a450e97833a1b02
usuario <usuario@10.22.146.203>
2013-11-22 11:24:30 +0000

    Con personLoader

--- src/katahistograma/Main.java
+++ src/katahistograma/Main.java
@@ -1,8 +1,28 @@
 package katahistograma;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
+        PersonLoader loader = createPersonLoader();
+        
+    }
 
+    private static PersonLoader createPersonLoader() {
+        return new PersonLoader() {
+
+            @Override
+            public Person[] load() {
+                ArrayList<Person> list = new ArrayList<>();
+                list.add(new Person("Luis", new Email("luis@gmail.com"), Sex.MALE));
+                list.add(new Person("Alba", new Email("alba@yahoo.fr"), Sex.FEMALE));
+                list.add(new Person("Tony", new Email("tony@gmail.com"), Sex.MALE));
+                list.add(new Person("Jackie", new Email("jackie@gmail.com"), Sex.FEMALE));
+                list.add(new Person("John", new Email("jfk@gmail.com"), Sex.MALE));
+                return list.toArray(new Person[list.size()]);
+                
+            }
+        };
     }
 }
---
+++ src/katahistograma/PersonLoader.java
@@ -0,0 +1,5 @@
+package katahistograma;
+
+public interface PersonLoader {
+    public Person[] load();
+}
