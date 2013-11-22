2063b1b9b803ab1421c5fc57087f18e2161232ba
usuario <usuario@10.22.146.203>
2013-11-22 12:17:47 +0000

    Librerias importadas

---
+++ src/katahistograma/ChartHistogramViewer.java
@@ -0,0 +1,14 @@
+package katahistograma;
+
+public class ChartHistogramViewer<T> extends HistogramViewer<T>{
+
+    public ChartHistogramViewer(Histogram<T> histogram) {
+        super(histogram);
+    }
+
+    @Override
+    public void show() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
